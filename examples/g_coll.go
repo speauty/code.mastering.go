036a9fc658b43c90d64e421a78bea21625d8524a
speauty <speauty@163.com>
2022-05-08 00:36:40 +0800

    补充gctrace注释;

--- examples/g_coll.go
+++ examples/g_coll.go
@@ -16,7 +16,13 @@ func printStats(mem runtime.MemStats) {
 
 /**
  * GODEBUG=gctrace=1 go run g_coll.go
+ * linux可视信息
  * gc 1 @0.004s 2%: 0.028+0.73+0.039 ms clock, 0.34+0.29/0.94/0+0.47 ms cpu, 4->4->0 MB, 4 MB goal, 0 MB stacks, 0 MB globals, 12 P
+ * gc 执行次数 程序耗时 垃圾回收时间占比 STW清扫时间+并发标记扫描时间+STW标记时间垃圾回收占CPU时间 GC前堆大小+GC后堆大小+存活堆大小 整体堆大小 整体栈大小 总大小 使用处理器数量
+ *
+ * windows可视信息
+ * gc 1 @0.005s 0%: 0+0.53+0 ms clock, 0+0/0.53/0.53+0 ms cpu, 47->48->0 MB, 48 MB goal, 12 P
+ * gc 执行次数 程序耗时 垃圾回收时间占比 STW清扫时间+并发标记扫描时间+STW标记时间垃圾回收占CPU时间 GC前堆大小+GC后堆大小+存活堆大小 整体堆大小 使用处理器数量
  */
 func main() {
 	var mem runtime.MemStats
